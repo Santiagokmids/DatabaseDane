b83053f5fa5503e2ff41646e7011861634c2b750
Luis Miguel Ossa Arias <1006016025@u.icesi.edu.co>
2022-01-27 23:55:19 -0500

    The new class Municipality whit his attributes has been added into the project

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
 
 namespace Database_Dane
 {
@@ -20,6 +22,8 @@ namespace Database_Dane
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,6 +32,27 @@ namespace Database_Dane
         private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+
+        private void Importar_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.ShowDialog();
+
+            if (openFile.ShowDialog() == true) 
+            {
+                string[] line = File.ReadAllLines(openFile.FileName);
+                List<Municipality> municipality = new List<Municipality>();
+
+                foreach (var linea in line) 
+                {
+                    var attributes = linea.Split(',');
+                    municipality.Add(new Municipality(attributes[0], attributes[1], attributes[2], attributes[3], attributes[4]));
+                }
+
+                ListViewItem listView = new ListViewItem();
+                list.Items.Add(municipality);
+            }
         }
     }
 }
