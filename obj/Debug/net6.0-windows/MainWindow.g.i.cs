ef3bc871a0541133e6b7628a915d0a43a52e11be
Luis Miguel Ossa Arias <1006016025@u.icesi.edu.co>
2022-01-28 14:16:36 -0500

    Some changes

--- obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -85,7 +85,7 @@ namespace Database_Dane {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Database Dane;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Database Dane;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
